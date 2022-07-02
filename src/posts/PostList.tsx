4fabd2f26967217034cf0ceaa14159367925c0a7
PsmD <tbvjeld7@naver.com>
2022-07-02 20:56:57 +0900

    feat: RootState type 추가 및 css 수정

--- src/posts/PostList.tsx
+++ src/posts/PostList.tsx
@@ -2,42 +2,55 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { deletePost } from "./postSlice";
 import { IPost } from "../types/post";
+import { RootState } from "../store";
+import styled from "styled-components";
 
 const PostList = () => {
 	const dispatch = useDispatch();
-	const posts = useSelector((store: any) => store.posts);
-
+	const posts = useSelector((state: RootState) => state.posts);
 	const handleRemovePost = (id: number) => {
 		dispatch(deletePost({ id }));
 	};
 
 	return (
-		<div>
+		<PostsBox>
 			<Link to="/add-post">
 				<button>Add Post</button>
 			</Link>
-			<div>
+			<div style={{ marginTop: "20px" }}>
 				{posts.length ? (
 					posts.map((post: IPost) => (
 						<div key={post.id}>
 							<div>
-								<h3>{post.title}</h3>
+								<h3>Title: {post.title}</h3>
 								<span>{post.text}</span>
 							</div>
-							<div>
+							<Buttons>
 								<Link to={`edit-post/${post.id}`}>
 									<button>edit</button>
 								</Link>
 								<button onClick={() => handleRemovePost(post.id)}>delete</button>
-							</div>
+							</Buttons>
 						</div>
 					))
 				) : (
 					<p>No Post</p>
 				)}
 			</div>
-		</div>
+		</PostsBox>
 	);
 };
 
 export default PostList;
+
+const PostsBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const Buttons = styled.div`
+	display: flex;
+	justify-content: end;
+	margin: 10px 0 50px 0;
+`;
